ab5810d8a6833d39d3c5846a673ba98f00a1fe44
zxzx9518 <zxzx9518@gmail.com>
2018-05-17 22:56:21 +0800

    234

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int integerone;
-	int integertwo;
-	int integerthree;
+	int integerOne;
+	int integerTwo;
+	int integerThree;
 	printf("請輸入第一個數字");
-	scanf("%d",&integerone);
+	scanf("%d",&integerOne);
 	printf("請輸入第二個數字");
-	scanf("%d",&integertwo);
-	integerthree=integerone+integertwo;
-	printf("2數字加起來的結果為: %d",integerthree);
+	scanf("%d",&integerTwo);
+	integerThree=integerone+integertwo;
+	printf("2數字加起來的結果為: %d",integerThree);
 	return 0;
 }
